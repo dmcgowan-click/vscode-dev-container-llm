8bff27a3dc147e5ed1dd9bddfdfb432f205cb8fe
Douglas McGowan <10809995+dmcgowandmc@users.noreply.github.com>
2026-07-04 03:10:43 +0000

    fix: Docker build multiarc support

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -15,8 +15,8 @@ if [ "" != '0' ]; then
     if [ "$(cat /etc/group | grep :${SOCKET_GID}:)" = '' ]; then sudoIf groupadd --gid ${SOCKET_GID} docker-host; fi 
     if [ "$(id ubuntu | grep -E "groups=.*(=|,)${SOCKET_GID}\(")" = '' ]; then sudoIf usermod -aG ${SOCKET_GID} ubuntu; fi
 fi
-exec "$@"
 
-# Start supervisord (dockers alternative to systemd)
-sudoIf supervisord -c /etc/supervisord.conf
+# Register QEMU binfmt handlers for cross-platform container builds
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes 2>/dev/null || true
+
 exec "$@"
